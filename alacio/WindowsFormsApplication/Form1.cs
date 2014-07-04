6633b596e54ae73791d4e0559c6f036c06e11033
gemma14 <gemmarosealacio@gmail.com>
2014-07-04 11:56:41 -0700

    csharp assignment

---
+++ alacio/WindowsFormsApplication/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication
+{
+    public partial class Form1 : Form
+    {
+        private Form2 theForm;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            theForm = new Form2();
+            theForm.setEveryThing(this, this.button1);
+            theForm.Show();
+        }
+    }
+}
---
+++ alacio/WindowsFormsApplication/Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication
+{
+    public partial class Form2 : Form
+    {
+        private Form1 theForm;
+        private Button button;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public void setEveryThing(Form1 myForm, Button theButton)
+        {
+            this.theForm = myForm;
+            this.button = theButton;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number;
+
+            try
+            {
+                number = int.Parse(textBox1.Text);
+                this.button.Text = (number + int.Parse(this.button.Text)).ToString();
+            }
+            catch (Exception ex) { }
+        }
+    }
+}
